781f487f5edb917da2bc6408f38957ca3b422002
emily_m <morin.emily@gmail.com>
2022-09-09 14:31:55 -0400

    Render jsx from modules

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
@@ -24,10 +17,22 @@ console.log(`We have ${foods.length} mouth-watering fruits.`);
 
 const randomFruit = choice(foods);
 
-console.log(`I'd like one ${randomFruit}, please!`);
-console.log(`Here you go: ${randomFruit}`);
-console.log("Delicious! May I have another?");
+// console.log(`I'd like one ${randomFruit}, please!`);
+// console.log(`Here you go: ${randomFruit}`);
+// console.log("Delicious! May I have another?");
 
 remove(foods, randomFruit);
 
-console.log(`I'm sorry, we're all out. We have ${foods.length} fruits left.`);
+// console.log(`I'm sorry, we're all out. We have ${foods.length} fruits left.`);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+        <p>We have {foods.length} mouth-watering fruits.</p>
+        <p>I'd like one {randomFruit}, please!</p>
+        <p>Here you go: {randomFruit}</p>
+        <p>We have {foods.length} fruits left.</p>
+    </div>
+   </React.StrictMode>
+);
