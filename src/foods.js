e85e5e66f38575afa9867cda659bb53de36e9e4e
emily_m <morin.emily@gmail.com>
2022-09-16 16:41:45 -0400

    Move fruits practice program out of index js

--- src/foods.js
+++ src/foods.js
@@ -1,3 +1,18 @@
+// -------- FRUITS MODULES EX --------
+import { choice, remove } from "./helpers";
+
 const foods = [ '🍇', '🍈', '🍉', '🍊', '🍋', '🍌', '🍍', '🍎', '🍏', '🍐', '🍒', '🍓', '🥝', '🍅', '🥑' ];
 
-export default foods;
+console.log(`We have ${foods.length} mouth-watering fruits.`);
+
+const randomFruit = choice(foods);
+
+// console.log(`I'd like one ${randomFruit}, please!`);
+// console.log(`Here you go: ${randomFruit}`);
+// console.log("Delicious! May I have another?");
+
+remove(foods, randomFruit);
+
+// console.log(`I'm sorry, we're all out. We have ${foods.length} fruits left.`);
+
+// export default foods;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-// import App from './App';
+import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 // If you want to start measuring performance in your app, pass a function
@@ -9,30 +9,11 @@ import ReactDOM from 'react-dom/client';
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-// -------- FRUITS MODULES EX --------
-import { choice, remove } from "./helpers";
-import foods from "./foods";
-
-console.log(`We have ${foods.length} mouth-watering fruits.`);
-
-const randomFruit = choice(foods);
-
-// console.log(`I'd like one ${randomFruit}, please!`);
-// console.log(`Here you go: ${randomFruit}`);
-// console.log("Delicious! May I have another?");
-
-remove(foods, randomFruit);
-
-// console.log(`I'm sorry, we're all out. We have ${foods.length} fruits left.`);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-        <p>We have {foods.length} mouth-watering fruits.</p>
-        <p>I'd like one {randomFruit}, please!</p>
-        <p>Here you go: {randomFruit}</p>
-        <p>We have {foods.length} fruits left.</p>
+      <App />
     </div>
    </React.StrictMode>
 );
